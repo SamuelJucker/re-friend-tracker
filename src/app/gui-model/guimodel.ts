619864f6574f7c8a5c2a55ad80787d72cc0df71b
SamuelJucker <juckesam@students.zhaw.ch>
2022-05-23 14:29:59 +0000

    *Halbfinal

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Friendtracker von Abel Ghebre und Samuel Jucker.",
+                                            "title": "Friendtracker von Samuel Jucker.",
             "formList": [
                 {
                     "id": "FriendForm",
